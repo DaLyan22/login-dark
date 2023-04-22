d4a216edcba48ef7dad8832d244538d080c798cf
abraao.junior abraaojr <gabrielgomessoares2007@gmail.com>
2023-04-22 16:00:03 -0300

    Tela de Login

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function luz(){
+    const corpo = document.getElementById('body')
+    corpo.style.backgroundColor = 'white'
+    
+}
+
+function dark(){
+  const fundo = document.getElementById('body')
+  fundo.style.backgroundColor = "#201b2c"
+}
